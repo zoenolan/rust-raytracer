30dbcee15c35e264c313f4a58ae58486c20a773f
Zoe Nolan <github@zoenolan.org>
2025-05-13 16:13:08 +0100

    Add a ray type

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 mod colour;
+mod ray;
 mod vec3;
 
 use colour::Colour;
+use ray::Ray;
 use std::io;
+use vec3::{Point3, Vec3};
+
+fn ray_color(r: &Ray) -> Colour {
+    let unit_direction = vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Colour::new(1.0, 1.0, 1.0) + t * Colour::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image size
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    const ASPECT_RATIO: f32 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
+
+    // Camera
+    let viewport_height = 2.0 as f32;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0 as f32;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin
+        - horizontal / 2.0 as f32
+        - vertical / 2.0 as f32
+        - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -16,11 +39,14 @@ fn main() {
         eprint!("\rScanlines remaining: {} ", j);
 
         for i in 0..IMAGE_WIDTH {
-            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.25 as f32;
+            let u = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let v = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
 
-            let pixel_colour = Colour::new(r, g, b);
+            let pixel_colour = ray_color(&r);
             colour::write_colour(&mut io::stdout(), pixel_colour);
         }
     }
