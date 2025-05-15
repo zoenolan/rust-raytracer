21bb3ed7806e655865ff83ca07a00fd6627fdf5d
Zoe Nolan <github@zoenolan.org>
2025-05-15 10:58:15 +0100

    Switch to a tighter FoV

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ fn main() {
         Point3::new(-2.0, 2.0, 1.0),
         Point3::new(0.0, 0.0, -1.0),
         Vec3::new(0.0, 1.0, 0.0),
-        90.0,
+        20.0,
         ASPECT_RATIO,
     );
 
